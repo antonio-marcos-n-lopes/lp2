7eff9e53fbedefa1e1df70f991cadffe56e01dff
Antonio Marcos <51960650+tunikinn@users.noreply.github.com>
2021-06-27 21:14:15 -0300

    Add files via upload

---
+++ Aula14/Compra.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace ATT_Antonio
+{
+	public class Compra:IImprimivel
+	{
+        protected Estoque _estoque;
+        protected Cliente _cliente;
+        protected Carrinho _carrinho;
+
+		public Estoque Estoque
+
+      {
+			get { 
+          return this._estoque;
+          }
+			set
+            {
+				_estoque = value;
+            }
+        }
+
+    public Carrinho Carrinho
+        {
+            get { return this._carrinho; }
+        }
+
+    public Cliente cliente
+        {
+            get { return this._cliente; }
+        }
+
+
+        public Compra (Cliente cliente, Carrinho carrinho, Estoque estoque)
+        {
+            this._carrinho = carrinho;
+            this._cliente = cliente;
+            this._estoque = estoque;
+		    }
+
+
+        public void Imprimir()
+        {
+            foreach(var itens in _estoque.Catalogo)
+
+            {
+                Cliente.Imprimir();
+                Carrinho.ImprimirCarrinho();
+            }
+        }
+
+    }
+}
---
+++ Aula14/Fornecedor.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace ATT_Antonio
+{
+	public class Fornecedor:IImprimivel
+	{
+		private string _Fornome;
+		private string _Forcontato;
+		private string _Forcnpj;
+		private string _Forendereco;
+
+
+		public string Nome
+        {
+			get { return this._Fornome; }
+        }
+    public string Contato
+		{
+			get { return this._Forcontato; }
+		}
+  	public string Cnpj
+		{
+			get { return this._Forcnpj; }
+		}
+		public string Endereco
+		{
+			get { return this._Forendereco; }
+		}
+		
+
+		public Fornecedor (string Fornome, string Forconato, string Forcnpj, string Forendereco)
+
+        {
+			this._Fornome = Fornome;
+			this._Forconato = Forendereco;
+			this._Forcnpj = Forcnpj;
+			this._Forendereco = Forcontato;
+        }
+
+
+		public void Imprimir()
+        {
+			Console.WriteLine("FORNECEDOR:\t{0}", this.Nome);
+      Console.WriteLine("CONTATO:\t{0}", this.Contato);
+      Console.WriteLine("CNPJ:\t{0}", this.Cnpj);
+			Console.WriteLine("ENDERÇO:\t{0}", this.Endereco);
+		}
+	}
+}
